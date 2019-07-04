b7e7b7e7f74e4fa5fc7423745b556946a088a0a0
Dylan <Dylanramper@outlook.com>
2019-07-03 17:56:05 -0700

    platforms working!!!

--- DungeonGame/GameScene.swift
+++ DungeonGame/GameScene.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
     var player = SKSpriteNode()
     var platforms: SKTileMapNode?
+    var background: SKTileMapNode!
     
     override func didMove(to view: SKView) {
         player = SKSpriteNode(imageNamed: "player")
@@ -20,9 +21,34 @@ class GameScene: SKScene {
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
         
         platforms = childNode(withName: "platforms") as? SKTileMapNode
+        
+        setupPlatforms()
+    }
+    
+    typealias TileCoordinates = (column: Int, row: Int)
+    
+    func tile(in tileMap: SKTileMapNode, at coordinates: TileCoordinates) -> SKTileDefinition?{
+        return tileMap.tileDefinition(atColumn: coordinates.column, row: coordinates.row)
     }
     
     func setupPlatforms(){
-    
+        guard let platforms = platforms else {return}
+        //1
+        var physicsbody = [SKPhysicsBody]()
+        //2
+        for row in 0..<platforms.numberOfRows{
+            for column in 0..<platforms.numberOfColumns{
+                guard let tile = tile(in: platforms, at: (column, row))
+                    else{continue}
+                //3
+                let center = platforms.centerOfTile(atColumn: column, row: row)
+                let body = SKPhysicsBody(rectangleOf: tile.size, center: center)
+                physicsbody.append(body)
+            }
+        }
+        //4
+        platforms.physicsBody = SKPhysicsBody(bodies: physicsbody)
+        platforms.physicsBody?.isDynamic = false
+        platforms.physicsBody?.friction = 0
     }
 }
--- Player.swift
+++ Player.swift
@@ -9,7 +9,16 @@
 import Foundation
 import SpriteKit
 
+enum PlayerSettings{
+    static let playerSpeed: CGFloat = 200.0
+    
+}
+
 class Player: SKSpriteNode {
+    
+    func move(target: CGPoint){
+        guard let physicsBody = physicsBody else{return}
+    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("Use Init()")
@@ -19,6 +28,12 @@ class Player: SKSpriteNode {
         let texture = SKTexture(imageNamed: "player")
         super.init(texture: texture, color: .white, size: texture.size())
         name = "Player"
+        
+        physicsBody = SKPhysicsBody(circleOfRadius: size.width/2)
+        physicsBody?.restitution = 1.0
+        physicsBody?.linearDamping = 1.0
+        physicsBody?.friction = 1.3
+        physicsBody?.allowsRotation = false
         
     }
 }
